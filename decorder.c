7fbc78abb4bbbe56989051e09d32b19c9c54a0a2
Henry-Cha <sungwon4728@naver.com>
2021-05-25 15:56:04 +0900

    decoder main틀

--- decorder.c
+++ decorder.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stldlib.h>
 
+int main(int argc, char* argv[])
+{
+	FILE* fpr(argv[0],"r+");  //인코딩 후 데이터 파일
+	FILE* fpw(argv[1],"w+");  //디코딩 후 데이터 파일
+/*
+	user();
+	items();
+	friend();
+	description();
+*/
+	fclose(fpr);
+	fclose(fpw);
+	return 0;
+}
