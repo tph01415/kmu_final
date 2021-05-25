06b166f83f93a4f63e6d314427db1053a6ed0799
tph01415 <juse0204@gmail.com>
2021-05-25 15:40:36 +0900

    encorder

--- encorder.c
+++ encorder.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*--------------
+ Userstatus - 구조체
+ 
+ 
+  --------------*/
 
+typedef struct user_status{
+	char id[255];
+	char name[255];
+	char gender;
+	int age;
+	char hp[20];
+	char mp[20];
+	char coin[20];
+}
+
+int main(int argc, char* argv[]){
+	FILE * fp;
+	fp = fopen(argv[1],"r");
+	
+
+}
