f6a54afecfb6c308eebf6d8235082328d13cf028
Henry-Cha <sungwon4728@naver.com>
2021-05-25 22:15:13 +0900

    주석 추가

--- decorder.c
+++ decorder.c
@@ -1,46 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TEXT_TYPE 1
+#define TEXT_TYPE 1  //소개글 글자타입
 #define NUM_TYPE 2
 
-FILE* description(FILE* fpr,FILE* fpw)
+FILE* description(FILE* fpr,FILE* fpw) //소개글 디코딩
 {
 	fprintf(fpw,"*DESCRIPTION*\n");
 	char tmp;
 	char c;
 	int a;
-	int check=0;
-	while((tmp = fgetc(fpr)) != EOF) 
+	int check=0;                          //문자인지 숫자인지 체크
+	while((tmp = fgetc(fpr)) != EOF)     //끝까지 1글자씩 읽어옴
 	{
-		if(tmp >= 'A' && tmp <= 'Z')
+		if(tmp >= 'A' && tmp <= 'Z')  //영문자일때
 		{
-			if(check == TEXT_TYPE)
+			if(check == TEXT_TYPE)  //이전 문자가 텍스트이면
 			{
-				fputc(c,fpw);
+				fputc(c,fpw);   //이전문자 출력
 			}
-			else if(check == NUM_TYPE)
+			else if(check == NUM_TYPE) // 이전문자 숫자이면
 			{
-				for(int i = 0;i<a;i++)
+				for(int i = 0;i<a;i++)   //숫자만큼 반복출력
 					fputc(c,fpw);
 			}
-			c = tmp;
-			check = TEXT_TYPE;
+			c = tmp;    //읽은문자 c에 저장
+			check = TEXT_TYPE;   //체크타입 변경
 		}
-		else if( tmp>='0' && tmp <='9')
+		else if( tmp>='0' && tmp <='9')    //숫자일때
 		{
-			if(check == TEXT_TYPE)
+			if(check == TEXT_TYPE)  //이전문자 텍스트
 			{
-				a = tmp-'0';
+				a = tmp-'0';  //읽은 숫자 a에 저장
 			}
-			if(check == NUM_TYPE)
+			if(check == NUM_TYPE) //이전문자 숫자일때
 			{
-				a=a*10;
+				a=a*10;        //두자리수 이상의 경우임
 				a+= tmp-'0';
 			}
-			check = NUM_TYPE;
+			check = NUM_TYPE;  //체크타입 변경
 		}
-		else if(tmp == '\n')
+		else if(tmp == '\n')  //줄바뀜
 		{
 			if(check == TEXT_TYPE)
 				fputc(c,fpw);
@@ -52,7 +52,7 @@ FILE* description(FILE* fpr,FILE* fpw)
 			fputc(tmp,fpw);
 			check = 0;
 		}
-		else
+		else              //특수문자(숫자텍스트)
 		{
 			if(check == TEXT_TYPE)
 			{
